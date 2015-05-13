f4f6b0fe7244032bd2f42d1e47b21b7187a17132
Nelson Yeung <nelsyeung@icloud.com>
2015-05-13 22:32:57 +0100

    Change indent to tabs instead of space

--- app/index.js
+++ app/index.js
@@ -4,67 +4,67 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-    initializing: function () {
-        this.pkg = require('../package.json');
-    },
+	initializing: function () {
+		this.pkg = require('../package.json');
+	},
 
-    prompting: function () {
-        // var done = this.async();
+	prompting: function () {
+		// var done = this.async();
 
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the well-made ' + chalk.red('nStatic') + ' generator!'
-        ));
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'Welcome to the well-made ' + chalk.red('nStatic') + ' generator!'
+		));
 
-        // var prompts = [{
-        //     type: 'confirm',
-        //     name: 'someOption',
-        //     message: 'Would you like to enable this option?',
-        //     default: true
-        // }];
+		// var prompts = [{
+		//     type: 'confirm',
+		//     name: 'someOption',
+		//     message: 'Would you like to enable this option?',
+		//     default: true
+		// }];
 
-        // this.prompt(prompts, function (props) {
-        //     this.someOption = props.someOption;
-        //
-        //     done();
-        // }.bind(this));
-    },
+		// this.prompt(prompts, function (props) {
+		//     this.someOption = props.someOption;
+		//
+		//     done();
+		// }.bind(this));
+	},
 
-    writing: {
-        app: function () {
-            this.fs.copy(
-                this.templatePath('_package.json'),
-                this.destinationPath('package.json')
-            );
-            this.fs.copy(
-                this.templatePath('_bower.json'),
-                this.destinationPath('bower.json')
-            );
-            this.directory(
-                this.templatePath('src'),
-                this.destinationPath('src')
-            );
-        },
+	writing: {
+		app: function () {
+			this.fs.copy(
+				this.templatePath('_package.json'),
+				this.destinationPath('package.json')
+			);
+			this.fs.copy(
+				this.templatePath('_bower.json'),
+				this.destinationPath('bower.json')
+			);
+			this.directory(
+				this.templatePath('src'),
+				this.destinationPath('src')
+			);
+		},
 
-        projectfiles: function () {
-            this.fs.copy(
-                this.templatePath('editorconfig'),
-                this.destinationPath('.editorconfig')
-            );
-            this.fs.copy(
-                this.templatePath('jshintrc'),
-                this.destinationPath('.jshintrc')
-            );
-            this.fs.copy(
-                this.templatePath('bowerrc'),
-                this.destinationPath('.bowerrc')
-            );
-        }
-    },
+		projectfiles: function () {
+			this.fs.copy(
+				this.templatePath('editorconfig'),
+				this.destinationPath('.editorconfig')
+			);
+			this.fs.copy(
+				this.templatePath('jshintrc'),
+				this.destinationPath('.jshintrc')
+			);
+			this.fs.copy(
+				this.templatePath('bowerrc'),
+				this.destinationPath('.bowerrc')
+			);
+		}
+	},
 
-    install: function () {
-        this.installDependencies({
-            skipInstall: this.options['skip-install']
-        });
-    }
+	install: function () {
+		this.installDependencies({
+			skipInstall: this.options['skip-install']
+		});
+	}
 });
